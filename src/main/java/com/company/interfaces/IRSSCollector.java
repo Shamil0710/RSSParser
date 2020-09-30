e048ecdc7ba7c4238952fc991f09408304aaf6b4
Shalim <Shamil0710@mail.ru>
2020-09-30 23:17:22 +0400

    Зразка новых классов

---
+++ src/main/java/com/company/interfaces/IRSSCollector.java
@@ -0,0 +1,17 @@
+package com.company.interfaces;
+
+import com.company.abstraction.AbstractRSSComponent;
+import com.company.parser.RSSComponent;
+import com.company.parser.RssRepository;
+
+import java.io.IOException;
+
+
+public interface IRSSCollector {
+
+    void collectRSSElements(RSSComponent component, RssRepository repository) throws IOException;
+
+    void toPrintAndSort(int numberOfLines, boolean sorting, String saveDirectory, RssRepository repository) throws IOException;
+
+
+}
