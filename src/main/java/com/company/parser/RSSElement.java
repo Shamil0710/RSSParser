10125db8b3375726f133956d75d7aa44716424a0
Shalim <Shamil0710@mail.ru>
2020-09-30 20:45:28 +0400

    Доблвен абстактный класс AbstractRSSCollector и создан интерфейс IRSSCollector. Изменена реализация метода compareTo

--- src/main/java/com/company/parser/RSSElement.java
+++ src/main/java/com/company/parser/RSSElement.java
@@ -5,7 +5,7 @@ import com.company.abstraction.AbstractRSSElement;
 
 import java.time.LocalDateTime;
 
-public class RSSElement extends AbstractRSSElement implements Comparable<RSSElement>{
+public class RSSElement extends AbstractRSSElement implements Comparable<RSSElement> {
 
     public RSSElement(String title, String url, LocalDateTime publicationDate, String componentUrl) {
 
