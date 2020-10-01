7f0036113a67945f9f6304c10f2550bc5c7c39c3
Shalim <Shamil0710@mail.ru>
2020-10-01 08:41:36 +0400

    Правка компоратора

--- src/main/java/com/company/parser/RSSElement.java
+++ src/main/java/com/company/parser/RSSElement.java
@@ -5,13 +5,15 @@ import com.company.abstraction.AbstractRSSElement;
 
 import java.time.LocalDateTime;
 
-public class RSSElement extends AbstractRSSElement implements Comparable<RSSElement> {
+public class RSSElement extends AbstractRSSElement {
 
     public RSSElement(String title, String url, LocalDateTime publicationDate, String componentUrl) {
 
         super(title, url, publicationDate, componentUrl);
 
     }
+
+
 
 }
 
